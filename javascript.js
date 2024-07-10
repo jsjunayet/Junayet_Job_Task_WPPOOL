17af24aaaeb00d1f531d7f90677cbc31e0ba950f
Junayet Shiblu <130874714+jsjunayet@users.noreply.github.com>
2024-07-10 19:11:54 +0600

    completed my task

---
+++ javascript.js
@@ -0,0 +1,458 @@
+
+const PotentailData = [
+    {
+        "img": "https://i.ibb.co/JRFmhpf/Group.png"
+    },
+    {
+        "img": "https://i.ibb.co/JyWqbqP/Group-227.png"
+    },
+    {
+        "img": "https://i.ibb.co/1my2xPY/Group-226.png"
+    },
+    {
+        "img": "https://i.ibb.co/d0XnMqp/Group-225.png"
+    },
+    {
+        "img": "https://i.ibb.co/cbPwTsW/Group-224.png"
+    },
+    {
+        "img": "https://i.ibb.co/TL4SbQL/Group-223.png"
+    },
+    {
+        "img": "https://i.ibb.co/dJByh3T/Group-222.png"
+    },
+    {
+        "img": "https://i.ibb.co/Cmsd7yN/Group-221.png"
+    },
+    {
+        "img": "https://i.ibb.co/gSk42hh/Group-220.png"
+    },
+    {
+        "img": "https://i.ibb.co/yh6h43J/Group-219.png"
+    }
+];
+const Companies = [
+
+    {
+        "img": "https://i.ibb.co/GHF2pBt/Vector.png"
+    },
+    {
+        "img": "https://i.ibb.co/TTmNqQm/ss-logo-2color-black-1.png"
+    },
+    {
+        "img": "https://i.ibb.co/qp5WgFq/nota-bene-logo-1.png"
+    },
+    {
+        "img": "https://i.ibb.co/cvGb4LP/quovo-logo.png"
+    },
+    {
+        "img": "https://i.ibb.co/ftMmfk5/Recurly-Logo-Purple-1.png"
+    },
+    {
+        "img": "https://i.ibb.co/KybZhRs/image-55.png"
+    },
+    {
+        "img": "https://i.ibb.co/Jy3gK4N/image-67-removebg-preview.png"
+    },
+    {
+        "img": "https://i.ibb.co/gJ544pf/kensho-social-card-2.png"
+    },
+    {
+        "img": "https://i.ibb.co/chqmNmY/image-52.png"
+    },
+    {
+        "img": "https://i.ibb.co/4SrV0G2/image-54.png"
+    },
+    {
+        "img": "https://i.ibb.co/KybZhRs/image-55.png"
+    },
+    {
+        "img": "https://i.ibb.co/VjjfLxM/Group-169.png"
+    },
+    {
+        "img": "https://i.ibb.co/BgjfRjb/image-50.png"
+    },
+    {
+        "img": "https://i.ibb.co/MVyRD4S/image-51.png"
+    },
+    {
+        "img": "https://i.ibb.co/k03dqvR/e92716bf-074b-46c2-bfdc-82dbfae1da06-2.png"
+    },
+    {
+        "img": "https://i.ibb.co/W2HQHQw/flywire.png"
+    },
+    {
+        "img": "https://i.ibb.co/V2c8X7f/Group-166.png"
+    },
+    {
+        "img": "https://i.ibb.co/9WNvnq2/62e84990058cefbfc33ef312-vendr-logo-1.png"
+    },
+    {
+        "img": "https://i.ibb.co/VTHp4cK/ae99cd49-2667-464e-b9db-b39cee0126e1-1.png"
+    },
+    {
+        "img": "https://i.ibb.co/Wt7CDHQ/argyle-black-l-2-removebg-preview.png"
+    }
+];
+ const Storiesdata = [
+    {
+        "img": "https://i.ibb.co/8KN1G73/image-98.png",
+        "Name": "By: Sahabuddin Sagor",
+        "des":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia."
+    },
+    {
+        "img": "https://i.ibb.co/6tZn9JL/image-97.png",
+        "Name": "By: Nayem Khan",
+        "des":"Key Takeaways from Robotics Invest 2023"
+    },
+    {
+        "img": "https://i.ibb.co/8KN1G73/image-98.png",
+        "Name": "By: Sahabuddin Sagor",
+        "des":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia."
+    },
+    {
+        "img": "https://i.ibb.co/6tZn9JL/image-97.png",
+        "Name": "By: Nayem Khan",
+        "des":"Key Takeaways from Robotics Invest 2023."
+    },
+    {
+        "img": "https://i.ibb.co/8KN1G73/image-98.png",
+        "Name": "By: Sahabuddin Sagor",
+        "des":"Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitia."
+    },
+    {
+        "img": "https://i.ibb.co/6tZn9JL/image-97.png",
+        "Name": "By: Nayem Khan",
+        "des":"Key Takeaways from Robotics Invest 2023."
+    },
+]
+let companiesData = [
+    {
+        Company: "Adyen",
+        Ticker: "AFRM",
+        Vertical: "Payments",
+        Price: "$17.15",
+        Market: "$50.5",
+        Revenue: '49.0%',
+        Gross: '15.8%',
+        Revenues: "6.3",
+        YTD: '24.3%'
+    },
+    {
+        Company: "Affirm",
+        Ticker: "AFRM",
+        Vertical: "Lending",
+        Price: "$16.12",
+        Market: "$4.7",
+        Revenue:"32.0%",
+        Gross: "48.7%",
+        Revenues: "4.9",
+        YTD: "77.1%"
+    },
+    {
+        Company: "Alkami Technology",
+        Ticker: "ALKT",
+        Vertical: "B2B SaaS",
+        Price: "$16.27",
+        Market: "$1.5",
+        Revenue:"34.0%",
+        Gross: "53.0%",
+        Revenues: "7.0",
+        YTD: "10.1%"
+    },
+    {
+        Company: "AvidXchange",
+        Ticker: "AVDX",
+        Vertical: "Payments",
+        Price: "$10.21",
+        Market: "$2.0",
+        Revenue:"27.0%",
+        Gross: "61.5%",
+        Revenues: "6.0",
+        YTD: "10.5%"
+    },
+   
+    {
+        Company: "Bakkt Holdings",
+        Ticker: "BKKT",
+        Vertical: "Wealth",
+        Price: "$1.32",
+        Market: "$0.1",
+        Revenue:"38%",
+        Gross: "--",
+        Revenues: "0.4",
+        YTD: "13.8%"
+    },
+    {
+        Company: "Virtu Financial Inc",
+        Ticker: "VIRT",
+        Vertical: "Wealth",
+        Price: "$17.0",
+        Market: "$3.1",
+        Revenue:"-22.0%",
+        Gross: "38.2%",
+        Revenues: "2.6",
+        YTD: "-17.8%"
+    },
+    {
+        Company: "Adyen",
+        Ticker: "AFRM",
+        Vertical: "Payments",
+        Price: "$17.15",
+        Market: "$50.5",
+        Revenue: '49.0%',
+        Gross: '15.8%',
+        Revenues: "6.3",
+        YTD: '24.3%'
+    },
+    {
+        Company: "Affirm",
+        Ticker: "AFRM",
+        Vertical: "Lending",
+        Price: "$16.12",
+        Market: "$4.7",
+        Revenue:"32.0%",
+        Gross: "48.7%",
+        Revenues: "4.9",
+        YTD: "77.1%"
+    },
+    {
+        Company: "Alkami Technology",
+        Ticker: "ALKT",
+        Vertical: "B2B SaaS",
+        Price: "$16.27",
+        Market: "$1.5",
+        Revenue:"34.0%",
+        Gross: "53.0%",
+        Revenues: "7.0",
+        YTD: "10.1%"
+    },
+    {
+        Company: "AvidXchange",
+        Ticker: "AVDX",
+        Vertical: "Payments",
+        Price: "$10.21",
+        Market: "$2.0",
+        Revenue:"27.0%",
+        Gross: "61.5%",
+        Revenues: "6.0",
+        YTD: "10.5%"
+    },
+  
+];
+function showConfirmationBox() {
+    var confirmationBox = document.getElementById('confirmationBox');
+    confirmationBox.style.display = 'flex'; // Show the confirmation box
+}
+
+function hideConfirmationBox() {
+    var confirmationBox = document.getElementById('confirmationBox');
+    confirmationBox.style.display = 'none'; // Hide the confirmation box
+}
+
+
+  
+window.onload = function() {
+    createImageCards();
+    createStoriesCards();
+    createPortfoiloCards();
+    renderCandidateTable();
+    var chartOptions = {
+        chart: {
+          height: 450,
+          type: 'line',
+          fontFamily: 'Helvetica, Arial, sans-serif',
+          foreColor: '#6E729B',
+          toolbar: {
+            show: false,
+          },
+        },
+        stroke: {
+          curve: 'straight',
+          width: 2,
+        },
+        series: [
+          {
+            name: 'WPPOOL',
+            data: [0, 15, 26, 20, 33, 27],
+            color: '#FC714D',
+          },
+          {
+            name: 'Google',
+            data: [0, 33, 21, 42, 19, 32],
+            color: '#615DE3',
+          },
+          {
+            name: 'Microsoft',
+            data: [0, 39, 52, 11, 29, 43],
+            color: '#AFCD80',
+          },
+          {
+            name: 'Twitter',
+            data: [0, 30, 50, 11, 10, 43],
+            color: '#FC714D',
+          },
+        ],
+        markers: {
+          size: 6,
+          strokeWidth: 0,
+          hover: {
+            size: 9,
+          },
+        },
+        grid: {
+          show: true,
+          padding: {
+            bottom: 0,
+          },
+        },
+        labels: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul'], // Corrected 'jul' to 'Jul'
+        xaxis: {
+          tooltip: {
+            enabled: true, // Ensure tooltip is enabled
+            formatter: function(val) {
+              return 'Date: June ' + (val + 1) + ', 2022'; // Custom tooltip format
+            }
+          },
+        },
+        yaxis: {
+          min: -10,
+          max: 100,
+          labels: {
+            formatter: function (val) {
+              return val + '%';
+            }
+          }
+        },
+        legend: {
+          position: 'bottom',
+          horizontalAlign: 'center',
+          offsetY: 10,
+          markers: {
+            shape: 'square',
+            radius: 4, 
+          },
+          labels: {
+            colors: '#373d3f',
+          },
+        },
+        grid: {
+          borderColor: '#D9DBF3',
+          xaxis: {
+            lines: {
+              show: true,
+            },
+          },
+        },
+      };
+
+    var lineChart = new ApexCharts(document.querySelector('#line-chart'), chartOptions);
+    lineChart.render();
+};
+
+  
+
+// Function to create image cards
+function createImageCards() {
+    const alldatasContainer = document.getElementById('alldatas');
+    PotentailData.forEach((item) => {
+        const card = document.createElement('div');
+        card.className = 'bg-[#FFFFFF] rounded-[6px] h-[120px]  flex flex-col justify-center items-center shadow-md';
+        const img = document.createElement('img');
+        img.className = 'object-fill';
+        img.src = item.img;
+        img.alt = 'Image';
+        card.appendChild(img);
+        alldatasContainer.appendChild(card);
+    });
+}
+
+// Function to create portfolio cards (assuming 'PortfoiloContainer' correction)
+function createPortfoiloCards() {
+    const PortfoiloContainer = document.getElementById('PortfoiloContainer');
+    Companies.forEach((item, i) => {
+        const card = document.createElement('div');
+        card.className = 'bg-[#FFFFFF] rounded-[6px] h-[120px]  flex flex-col justify-center items-center shadow-md';
+        const img = document.createElement('img');
+        img.className = 'object-fill';
+        img.src = item.img;
+        img.alt = `Image ${i}`;
+        card.appendChild(img);
+        PortfoiloContainer.appendChild(card);
+    });
+}
+
+// Function to create stories cards
+function createStoriesCards() {
+    const storiesContainer = document.getElementById('stories');
+    let html = '';
+
+    Storiesdata.forEach((item) => {
+        html += `
+            <div class="rounded-[8px] relative bg-[#FFFFFF]">
+                <img src="${item.img}" alt="Image" class="object-fill absolute -top-12 left-2">
+                <div class="text-white p-[19px]">
+                    <p class="text-[#2042B6] text-[14px] ${item.Name === 'By: Nayem Khan' ? 'mt-[90px]' : 'mt-16'}">${item.Name}</p>
+                    <p class=" text-[#191618]">${item.des}</p>
+                    <button class="rounded-2xl mt-3 bg-[#2042B6] text-[#fff] py-[6px] px-3">Read More</button>
+                </div>
+            </div>
+        `;
+    });
+
+    storiesContainer.innerHTML = html;
+}
+
+// Function to render candidate table
+function renderCandidateTable() {
+    const candidateTableBody = document.getElementById('candidateTableBody');
+    let html = '';
+
+    companiesData.forEach((item, index) => {
+        html += `
+            <tr class="${index % 2 === 0 ? 'bg-gray-100' : 'bg-white'}">
+                <td class="md:px-5 px-1 py-3 md:text-base text-[10px]">${item.Company}</td>
+                <td class="md:text-base text-[10px]">${item.Ticker}</td>
+                <td class="md:text-base text-[10px]">${item.Vertical}</td>
+                <td class="md:text-base text-[10px]">${item.Price}</td>
+                <td class="md:text-base text-[8px]">${item.Market}</td>
+                <td class="md:text-base text-[8px]">${item.Revenue}</td>
+                <td class="md:text-base text-[8px]">${item.Gross}</td>
+                <td class="md:text-base text-[8px]">${item.Revenues}</td>
+                <td class="md:text-base text-[8px]">${item.YTD}</td>
+            </tr>
+        `;
+    });
+
+    candidateTableBody.innerHTML = html;
+}
+// This is script file
+
+$('.testimonials-container').owlCarousel({
+
+    margin:-90,
+    nav:true,
+    navText:["<i class='fa-solid fa-arrow-left'></i>",
+             "<i class='fa-solid fa-arrow-right'></i>"],
+    responsive:{
+        0:{
+            items:1,
+            nav:false,
+            margin:10
+        },
+        600:{
+            items:1,
+            nav:true,
+            margin:10
+        },
+        768:{
+            items:2
+        },
+    }
+})
+
+
+  
+
+
+
